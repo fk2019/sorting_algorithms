c4efa3461fb02c4c8165cf9537ca6e799683f8d2
fk2019 <fkmuiruri8@gmail.com>
2023-01-30 08:34:16 -0800

    betty checks

--- sort.h
+++ sort.h
@@ -4,11 +4,18 @@
 #include <stdlib.h>
 #define UP 0
 #define DOWN 1
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 /* enum bool - enumerate bool*/
 typedef enum bool
@@ -19,12 +26,5 @@ typedef enum bool
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
 
 #endif /* _SORT_H_*/
