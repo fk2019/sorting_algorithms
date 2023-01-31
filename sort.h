05629f85ffe64655852c2628bc861d648272cf6d
fk2019 <fkmuiruri8@gmail.com>
2023-01-31 07:29:03 -0800

    Add 100

--- sort.h
+++ sort.h
@@ -33,4 +33,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+void shell_sort(int *array, size_t size);
 #endif /* _SORT_H_*/
